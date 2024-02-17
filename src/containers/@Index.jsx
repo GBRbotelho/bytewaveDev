6fd951f98409c96b2106da2969078df82fe80427
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2024-02-17 02:34:24 -0300

    Container 7

--- src/containers/@Index.jsx
+++ src/containers/@Index.jsx
@@ -9,6 +9,7 @@ import Container3 from "./Container3";
 import Container4 from "./Container4";
 import Container5 from "./Container5";
 import Container6 from "./Container6";
+import Container7 from "./Container7";
 
 export default function Index() {
   return (
@@ -24,6 +25,7 @@ export default function Index() {
         <Container5 />
         <Container6 />
       </div>
+      <Container7 />
     </main>
   );
 }
---
+++ src/containers/Container7.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./Container7.module.css";
+
+import Logo from "../assets/imgs/Bytewave.png";
+
+export default function Container7() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <img src={Logo} alt="Logo Bytewave" />
+        </div>
+        <div className={styles.dataDiv}>
+          <div className={styles.contact}>
+            <span>Email</span>
+            <p>Contato@bytewavedev.com.br</p>
+            <span>Whatsapp</span>
+            <a href="https://wa.me/19998631040">
+              <p>+55 19 9 9863-1040</p>
+            </a>
+          </div>
+          <div className={styles.social}>
+            <span>Redes sociais</span>
+            {/* <p>Facebook</p> */}
+            <a href="https://www.instagram.com/bytewavedev/">
+              <p>Instagram</p>
+            </a>
+            {/* <p>Linkedin</p> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
