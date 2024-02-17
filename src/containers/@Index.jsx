a30b245c8ff711788fb9d720857c167c47f3f4d4
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2024-02-17 02:00:11 -0300

    Container 4

--- src/containers/@Index.jsx
+++ src/containers/@Index.jsx
@@ -6,6 +6,7 @@ import Navbar from "./@Navbar";
 import Container1 from "./Container1";
 import Container2 from "./Container2";
 import Container3 from "./Container3";
+import Container4 from "./Container4";
 
 export default function Index() {
   return (
@@ -16,6 +17,7 @@ export default function Index() {
       </div>
       <Container2 />
       <Container3 />
+      <Container4 />
     </main>
   );
 }
---
+++ src/containers/Container4.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Container4.module.css";
+
+import icon from "../assets/imgs/7.svg";
+
+export default function Container4() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <p>Esta na duvida do qual dessas soluções você realmente precisa?</p>
+        </div>
+        <div className={styles.button}>
+          <a href="https://wa.me/19974026227">
+            <button>
+              <img src={icon} alt="Icone Especialista" />
+              conversar com especialista
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
